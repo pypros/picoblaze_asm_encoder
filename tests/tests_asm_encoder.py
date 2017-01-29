3d0cd41a062f45bab62e889cb806ef73073033b9
pypros <sobczuk.lukasz@wp.pl>
2017-01-29 19:42:54 +0100

    Implementation encoding ADDCY instruction and tests.

--- tests/tests_asm_encoder.py
+++ tests/tests_asm_encoder.py
@@ -1,6 +1,6 @@
 from asm_encoder import PicoBlazeAsmEncoder
 
-def test_encode_operation_ADD__sx_kk():
+def test_encode_operation_ADD_sx_kk():
     line_of_code = "ADD s0, 127"
     #         "011000xxxxkkkkkkkk"
     binary_instruction_expected = "011000000001111111"  # ADD s0, kk
@@ -8,16 +8,30 @@ def test_encode_operation_ADD__sx_kk():
     asm_encoder.encode_instruction(line_of_code)
     assert binary_instruction_expected == asm_encoder._PicoBlazeAsmEncoder__encoded_instruction_to_bin
 
-def test_encode_operation_ADD__sx_sy():
+def test_encode_operation_ADD_sx_sy():
     line_of_code = "ADD s0, sF"
-    #         "011000xxxxkkkkkkkk"
-    binary_instruction_expected = "011001000011110000"  # ADD s0, kk
+    #         "011001xxxxyyyy0000"
+    binary_instruction_expected = "011001000011110000"  # ADD s0, sF
     asm_encoder = PicoBlazeAsmEncoder()
     asm_encoder.encode_instruction(line_of_code)
     assert binary_instruction_expected == asm_encoder._PicoBlazeAsmEncoder__encoded_instruction_to_bin
 
-def test_encode_operation_ADDCY():
-    pass
+def test_encode_operation_ADDCY_sx_kk():
+    line_of_code = "ADDCY s0, 127"
+    #         "011010xxxxkkkkkkkk"
+    binary_instruction_expected = "011010000001111111"  # ADDCY s0, kk
+    asm_encoder = PicoBlazeAsmEncoder()
+    asm_encoder.encode_instruction(line_of_code)
+    assert binary_instruction_expected == asm_encoder._PicoBlazeAsmEncoder__encoded_instruction_to_bin
+
+def test_encode_operation_ADDCY_sx_sy():
+    line_of_code = "ADDCY s0, sF"
+    #         "011011xxxxyyyy0000"
+    binary_instruction_expected = "011011000011110000"  # ADDCY s0, sF
+    asm_encoder = PicoBlazeAsmEncoder()
+    asm_encoder.encode_instruction(line_of_code)
+    assert binary_instruction_expected == asm_encoder._PicoBlazeAsmEncoder__encoded_instruction_to_bin
+
 def test_encode_operation_CALL():
     pass
 def test_encode_operation_COMPARE():
